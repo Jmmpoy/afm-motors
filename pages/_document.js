e35f0ffa908d8af71702988b2a8bb67705b4b116
Jmmpoy <dunker35@hotmail.fr>
2023-05-08 18:47:23 +0200

    FAVICON

--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,44 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import React from 'react'
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
           <link
-        rel="preload"
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
-        
-        crossOrigin=""
-      />
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/public/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/public/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/public/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/public/favicons/site.webmanifest" />
+          <link rel="mask-icon" href="/public/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff"></meta>
+          <link
+            rel="preload"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
+            crossOrigin=""
+          />
         </Head>
-        
+
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
